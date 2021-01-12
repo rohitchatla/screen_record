a3290b9f782e078fc9f8fa6dfc54535a7f4ea99a
CV Rohit <51963891+rohitchatla@users.noreply.github.com>
2021-01-12 08:11:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,26 @@ const video = document.querySelector("video");
 let recorder, stream;
 
 async function startRecording() {
-  stream = await navigator.mediaDevices.getDisplayMedia({
-    video: { mediaSource: "screen" }
+  stream = await navigator.mediaDevices.getDisplayMedia({//for screen and with screen sound
+    video: { 
+    		mediaSource: "screen",
+    		 cursor: "always" 
+    		},
+    audio: true
+    //audio: { :true}
   });
-  recorder = new MediaRecorder(stream);
+
+  // stream = await navigator.mediaDevices.getUserMedia({//for webcam and audio
+  //   video: true,
+  //   audio: true
+  // });
+  var options = {mimeType: 'video/webm; codecs=vp9'};//{mimeType: 'video/mp4; codecs=vp9'}--> is not supported
+  recorder = new MediaRecorder(stream, options);
 
   const chunks = [];
   recorder.ondataavailable = e => chunks.push(e.data);
   recorder.onstop = e => {
-    const completeBlob = new Blob(chunks, { type: "video\/mp4" });//type: chunks[0].type 
+    const completeBlob = new Blob(chunks, { type: chunks[0].type });//type: chunks[0].type {type: "video\/mp4"}
     video.src = URL.createObjectURL(completeBlob);
   };
 
